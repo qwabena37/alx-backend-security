95d9d25f1f0804b7786e53302e51e931639866c5
James <qjaymce37@gmail.com>
2025-11-08 19:44:57 +0000

    add README.md

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,16 @@
+from .models import RequestLog
+from django.utils.deprecation import MiddlewareMixin
+
+class IPLoggingMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        ip = self.get_client_ip(request)
+        path = request.path
+        RequestLog.objects.create(ip_address=ip, path=path)
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
